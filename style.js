b8dc8cdef0458bfa1bb0585d4a3a497e14d7e414
AntonelloDeGiosa <163391602+AntonelloDeGiosa@users.noreply.github.com>
2025-02-17 20:07:54 +0100

    agg_js

---
+++ style.js
@@ -0,0 +1,17 @@
+document.getElementById('btn').addEventListener('click', function(e) {
+    e.preventDefault();     
+
+    const categoria = document.getElementById('option').value;
+    let url = 'https://api.chucknorris.io/jokes/random';
+
+    if (categoria !== '0') {
+        url += `?category=${categoria}`;
+    }
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('battuta').innerHTML = `<p>${data.value}</p>`;
+        })
+        .catch(error => console.error('Error fetching joke:', error));
+}); 
